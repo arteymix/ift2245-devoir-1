8e7c78b41c195c911c6165749254392ddb21ac4e
DjAntaki <antaki.vincent@gmail.com>
2015-02-09 12:48:22 -0500

    Pas fini. Le useradd demande une entrée cryptée pour le password (y'a d'lair d'avoir une commande qui sappel crypt). et je sais pas comment placer l'output de ptree dans un fichier.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env sh
 
-g++ -std=c++11 ptree.cpp -o ptree
+useradd $1 $2
+mkdir -p /home/$1/tp1/{src,bin,result}
+cp ptree.cpp /home/$1/tp1/src
+chown -r $1 /home/$1/* 
+
+pushd /home/$1/tp1
+
+g++ -std=c++11 src/ptree.cpp bin -o ptree
+chmod 777 bin/ptree
+export PATH=$PATH:/bin/ptree
+
+# Executer ptree et mettre dans result/zero
+# Executer ptree 1 2 et mettre dans result/one_two
+#ajouter txt auteur avec nos noms et da dans home/$1
+
+popd
