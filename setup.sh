2d949528cced797ba5d3febcf943686f523c6e49
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2015-02-10 15:53:49 -0500

    Améliorations pour le script setup.sh
    La portion est exécuter en se substituant à l'utilisateur par la
    commande su.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,42 @@
 #!/usr/bin/env sh
+
+set -e
+
 user="$1"
-useradd user "$2" #faut crypte $2 non?
-mkdir -p /home/$user/tp1/{src,bin,result}
-cp ptree.cpp /home/$user/tp1/src
-chown -r $user /home/$user/* 
+password="$2"
 
-pushd /home/$user/tp1
+if [ -z "$user" -o -z "$password" ]; then
+    echo 'usage: setup.sh <username> <password>'
+    exit 1
+fi
 
-g++ -std=c++11 src/ptree.cpp bin -o ptree
-chmod 777 bin/ptree
-export PATH=$PATH:/bin/ptree
+useradd -m $user
+chpasswd $user:$password
+
+# copie des fichiers dans le dossier utilisateur
+cp ptree.cpp setup.sh /home/$user
+
+# La commande suivante est inspirée d'une réponse sur StackOverflow
+# http://stackoverflow.com/questions/23929235/bash-multi-line-string-with-extra-space
+su $user << EOM
+
+cd $HOME
+
+# hiéarchie de dossiers
+mkdir -p tp1/{src,bin,result}
+
+cd tp1
+
+# copie des sources
+cp ptree.cpp setup.sh tp1/src
+
+g++ src/ptree.cpp bin -o bin/ptree
+
+export PATH=$PATH:$HOME/bin
+
 ptree > result/zero
 ptree 1 2 > result/one_two
 
-#La commande suivante a été prise sur StackOverflow :
-# 
-cat > Auteur.txt <<- EOM
-Vincent Antaki (p1038646),
-Guillaume Poirier-Morency (p1053380)
+echo Vincent Antaki (p1038646) > Auteur.txt
+echo Guillaume Poirier-Morency (p1053380) >> Auteur.txt
 EOM
-popd
-
